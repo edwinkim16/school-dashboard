14201d78bce6866c3f0e5c0d44a327bb6b986bfe
edwinkim16 <edwinkimathi226@gmail.com>
2025-11-27 21:06:10 +0300

    setting up navbar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -22,7 +22,10 @@ export default function RootLayout({
         <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">R</div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }

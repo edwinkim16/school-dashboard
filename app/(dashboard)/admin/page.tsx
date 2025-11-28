20b404a122eb92d19d71d81d833df186d9381159
edwinkim16 <edwinkimathi226@gmail.com>
2025-11-28 11:01:08 +0300

    changed colour and updated admin page

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,8 +1,24 @@
-
+import UserCard from "@/components/UserCard";
 
 const Admin = () => {
   return (
-    <div>Admin</div>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 ">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between ">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">
+       r
+      </div>
+    </div>
   )
 }
 
